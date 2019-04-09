4c0fbab60527cb842c3405b0f3fd141cb69b3750
vijaymirkale <49389560+vijaymirkale@users.noreply.github.com>
2019-04-09 14:00:39 +0530

    Update TestSorting.java

--- src/main/java/com/scp/sorting/TestSorting.java
+++ src/main/java/com/scp/sorting/TestSorting.java
@@ -5,5 +5,7 @@ import javax.swing.plaf.synth.SynthStyle;
 public class TestSorting {
 		public static void main(String[] args) {
 		System.out.println("git push");
+		System.out.println("git change");
+			
 		}
 }
